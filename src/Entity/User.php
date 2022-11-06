86c0c37e24126f5788955181655ab45967e396b0
Eduardo SA E SILVA <saesilva.e@gmail.com>
2022-11-06 17:33:41 +0100

    correctif 1

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -20,6 +20,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 180, unique: true)]
     private ?string $email = null;
 
+    #[ORM\Column]
     private ?int $roles = null;
 
     /**
