b5022f26aa705669b31e0e0343a419931926ae8c
levyf2i <105145981+levyf2i@users.noreply.github.com>
2023-01-02 14:33:07 +0100

    reservation sans prix

--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -7,17 +7,21 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+
 class ReservationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('DateDebut')
-            ->add('DateFin')
-            ->add('NbNuit')
-            ->add('Total')
-            ->add('Statue')
-            ->add('StatuePayment')
+        ->add('DateDebut',DateType::class,['attr' => ['class' => 'form-control date']])
+        ->add('DateFin',DateType::class,['attr' => ['class' => 'form-control date']])
+        ->add('NbNuit',IntegerType::class,['attr' => ['class' => 'form-control nb d-none','min' => 1,'value' => '0']])
+        ->add('Total',IntegerType::class,['attr' => ['class' => '','min' => 1,'value' => '1']])
+        ->add('Statue',IntegerType::class,['attr' => ['class' => 'd-none','value' => '0']])
+        ->add('StatuePayment',IntegerType::class,['attr' => ['class' => 'disabled','min' => 1,'value' => '1']])
         ;
     }
 
