b35d72bd6ae0fe2e4e8cce7f3626e10af895a93b
levyf2i <105145981+levyf2i@users.noreply.github.com>
2023-01-20 16:25:38 +0100

    Update LebelleOptionAnnonceType.php

--- src/Form/LebelleOptionAnnonceType.php
+++ src/Form/LebelleOptionAnnonceType.php
@@ -6,14 +6,15 @@ use App\Entity\LebelleOptionAnnonce;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class LebelleOptionAnnonceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('value')
-            ->add('status')
+            ->add('value',TextType::class,['attr' => ['class' => 'form-control '],'label' => 'Nom du champs personaliser'])
+            ->add('status',TextType::class,['attr' => ['class' => 'form-control '],'label' => '1 obligatoire 0 non obligatoire'])
             
         ;
     }
